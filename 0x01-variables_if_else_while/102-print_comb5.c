fb14b2cf116052133ffcbea79416af43106a5d9b
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-03-17 09:51:22 -0700

    print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,9 +12,9 @@ int main(void)
 	{
 		for (j = 48; j < 57; j++)
 		{
-			for (k = 48; k <= 57; k++)
+			for (k = i; k <= 57; k++)
 			{
-				for (l = 49; l <= 57; l++)
+				for (l = j + 1; l <= 57; l++)
 				{
 					putchar(i);
 					putchar(j);
