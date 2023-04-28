9d52e8414f8ad752c5cf34f465e7ce16bee97c2a
LeoYoumbi <Leoyoumbi83@gmail.com>
2023-04-28 16:12:06 +0100

    Fix betty: 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -26,7 +26,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (newNode);
 	}
 
-	*curNode = *head;
+	curNode = *head;
 	while (curNode->next)
 		curNode = curNode->next;
 	curNode->next = newNode;
