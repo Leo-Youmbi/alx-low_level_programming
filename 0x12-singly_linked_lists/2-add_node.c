235ed064c90d505de6f649c6a58e5dab100b8cf7
LeoYoumbi <Leoyoumbi83@gmail.com>
2023-04-28 15:22:55 +0100

    Ex: Singly linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,7 +13,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *neww;
 
-	neww = malloc(sizeof(list_h));
+	neww = malloc(sizeof(list_t));
 	if (!neww)
 		return (NULL);
 	neww->str = strdup(str;
